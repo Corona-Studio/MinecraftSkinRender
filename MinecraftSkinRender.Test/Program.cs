bc73a58b16c30d2f02325f25bad42cd91034d490
Color_yr <402067010@qq.com>
2025-04-29 11:10:31 +0800

    up add cape

--- MinecraftSkinRender.Test/Program.cs
+++ MinecraftSkinRender.Test/Program.cs
@@ -34,12 +34,14 @@ using SkiaSharp;
 //}
 
 using var skin = SKBitmap.Decode("skin.png");
+using var cape = SKBitmap.Decode("cape.png");
 
-//Skin2DHeadTypeA.MakeHeadImage(skin).SavePng("tempa.png");
-//Skin2DHeadTypeB.MakeHeadImage(skin).SavePng("tempb.png");
-//Skin3DHeadTypeA.MakeHeadImage(skin).SavePng("tempc.png");
-//Skin3DHeadTypeB.MakeHeadImage(skin, 15, 65).SavePng("tempd.png");
+Skin2DHeadTypeA.MakeHeadImage(skin).SavePng("tempa.png");
+Skin2DHeadTypeB.MakeHeadImage(skin).SavePng("tempb.png");
+Skin3DHeadTypeA.MakeHeadImage(skin).SavePng("tempc.png");
+Skin3DHeadTypeB.MakeHeadImage(skin, 15, 65).SavePng("tempd.png");
 Skin2DTypeA.MakeSkinImage(skin).SavePng("tempe.png");
 Skin2DTypeB.MakeSkinImage(skin).SavePng("tempf.png");
 Skin2DTypeA.MakeSkinImage(skin, SkinType.NewSlim).SavePng("tempg.png");
 Skin2DTypeB.MakeSkinImage(skin, SkinType.NewSlim).SavePng("temph.png");
+Cape2DTypaA.MakeCapeImage(cape).SavePng("tempi.png");
