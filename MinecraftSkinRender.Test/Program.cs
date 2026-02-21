e1ed91064c5923b4b66871a10013a083939b151d
Color_yr <402067010@qq.com>
2026-02-21 12:00:11 +0800

    up

--- MinecraftSkinRender.Test/Program.cs
+++ MinecraftSkinRender.Test/Program.cs
@@ -2,36 +2,7 @@
 using MinecraftSkinRender.Image;
 using SkiaSharp;
 
-//Console.WriteLine("Download skin");
-
-//var res = await MinecraftAPI.GetMinecraftProfileNameAsync("Color_yr");
-//var res1 = await MinecraftAPI.GetUserProfile(res!.UUID);
-//TexturesObj? obj = null;
-//foreach (var item in res1!.properties)
-//{
-//    if (item.name == "textures")
-//    {
-//        var temp = Convert.FromBase64String(item.value);
-//        var data = Encoding.UTF8.GetString(temp);
-//        obj = JsonConvert.DeserializeObject<TexturesObj>(data);
-//        break;
-//    }
-//}
-//if (obj == null)
-//{
-//    Console.WriteLine("No have skin");
-//    return;
-//}
-//if (obj!.textures.SKIN.url != null)
-//{
-//    var data = await MinecraftAPI.Client.GetByteArrayAsync(obj!.textures.SKIN.url);
-//    File.WriteAllBytes("skin.png", data);
-//}
-//if (obj.textures.CAPE.url != null)
-//{
-//    var data = await MinecraftAPI.Client.GetByteArrayAsync(obj!.textures.CAPE.url);
-//    File.WriteAllBytes("cape.png", data);
-//}
+await SkinDownloader.Download();
 
 using var skin = SKBitmap.Decode("skin.png");
 using var cape = SKBitmap.Decode("cape.png");
